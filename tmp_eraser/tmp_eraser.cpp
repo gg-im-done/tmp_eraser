5352f5ccd114470edf6223d01ae3035e93081388
gg-im-done <asketid@gmail.com>
2023-07-28 18:16:15 +0300

    upd

--- tmp_eraser/tmp_eraser.cpp
+++ tmp_eraser/tmp_eraser.cpp
@@ -3,21 +3,22 @@
 #include <vector>
 #include <array>
 #include <filesystem>
+#include <io.h>
 
 #define Log(text) std::wcout << text << L"\n"
 #define Error(text) std::wcerr << L"ERROR | " << text << L"\n"
 
 bool delete_file(const std::wstring& full_path, bool display_logs = false)
 {
+   static std::error_code error_info;
    std::filesystem::path file_path(full_path);
-   std::error_code ec;
-   if (bool not_removed = !std::filesystem::remove(file_path, ec))
+   if (bool not_removed = !std::filesystem::remove(file_path, error_info))
    {
       if (display_logs)
       {
-         std::string msg = ec.message();
-         std::wstring wstr_message(msg.begin(), msg.end());
-         Error(wstr_message << L" | " << full_path);
+         std::string message_str = error_info.message();
+         std::wstring message_wstr(message_str.begin(), message_str.end());
+         Error(message_wstr << L" | " << full_path);
       }
       return false;
    }
@@ -41,17 +42,17 @@ size_t delete_all(const std::vector<std::wstring>& file_paths)
 bool get_tmp_dir(std::filesystem::path& directory_path)
 {
    wchar_t tmp_path[128];
-   size_t len;
+   size_t len = 0;
    [[maybe_unused]] auto fuck = _wgetenv_s(&len, tmp_path, L"TMP");
    if (0 == len)
    {
-      Error("Cannot read TMP environment variable");
+      Error(L"Cannot read TMP environment variable");
       return false;
    }
    directory_path = tmp_path;
    if (!std::filesystem::exists(directory_path) || !std::filesystem::is_directory(directory_path))
    {
-      Error("Invalid TMP path");
+      Error(L"Invalid TMP path");
       return false;
    }
    return true;
@@ -73,12 +74,14 @@ size_t find_cl_files(std::vector<std::wstring>& file_paths, const std::filesyste
          Log(file_path);
       }
    }
-   Log("Total files: " << file_paths.size());
+   Log(L"Total files: " << file_paths.size());
    return file_paths.size();
 }
 
 int main()
 {
+   [[maybe_unused]] auto o_O = _setmode(_fileno(stdout), 0x20000);
+
    std::filesystem::path directory_path;
    if (!get_tmp_dir(directory_path))
       return 1;
@@ -86,15 +89,22 @@ int main()
    std::vector<std::wstring> file_paths;
    file_paths.reserve(4000);
    if (!find_cl_files(file_paths, directory_path))
-      return 1;
+   {
+      Log(L"no CL files found, quitting...");
+      return 2;
+   }
 
-   char decision;
-   std::cout << "Delete? (y): ";
+   std::wcout << L"Delete? (y): ";
+   char decision{ 0 };
    std::cin >> decision;
-   if (decision != 'y')
-      return 0;
-
-   size_t count = delete_all(file_paths);
-   Log(count << " files deleted.");
+   if (decision == 'y')
+   {
+      auto count = delete_all(file_paths);
+      Log(count << L" files deleted.");
+   }
+   else
+   {
+      Log(L"\nOk. Quitting... \\o/");
+   }
    return 0;
 }
