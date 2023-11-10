541f8eb7fd0514282a59480960acadcee5b02344
gg-im-done <asketid@gmail.com>
2023-11-10 12:12:47 +0200

    made everything worse

--- tmp_eraser/tmp_eraser.cpp
+++ tmp_eraser/tmp_eraser.cpp
@@ -1,71 +1,70 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <array>
 #include <filesystem>
-#include <format>
+#include <print>
 #include <string_view>
+#include <map>
 
-#ifdef _WIN32
-#include <io.h>
-__forceinline auto EnableUnicodeOutput() noexcept
-{
-   [[maybe_unused]] const auto o_O = _setmode(_fileno(stdout), 0x20000);
-}
-#else
-void EnableUnicodeOutput() noexcept
-{
-   // Linux
-}
-#endif
+static constexpr bool VERBOSE{ true };
 
-static constexpr bool VERBOSE{ false };
 
-void Print(auto thing)
-{
-   std::wcout << thing << L"\n";
-}
 
-void PrintError(auto thing)
+class EnvironmentVariableBank
 {
-   std::wcout << L"[ERROR] " << thing << L"\n";
-}
-
-bool GetPathTmpSysDir(std::filesystem::path& pathTmpSysDir)
-{
-   std::string sTmpEnvVar(std::getenv("TMP"));
-   pathTmpSysDir = sTmpEnvVar;
-   Print(sTmpEnvVar.c_str());
-   if (!std::filesystem::exists(pathTmpSysDir) || !std::filesystem::is_directory(pathTmpSysDir))
+   std::map<std::string, std::filesystem::path> m_envVars;
+public:
+   EnvironmentVariableBank(std::string_view sName)
    {
-      PrintError(L"Invalid TMP path");
-      return false;
+      FindVariable(sName);
    }
-   return true;
-}
 
-[[nodiscard]] bool DeleteFile(std::wstring_view sFilePath)
-{
-   static std::error_code err;
-   if (std::filesystem::path pathFile(sFilePath); !std::filesystem::remove(pathFile, err))
+   bool FindVariable(std::string_view sName)
    {
-      if constexpr (VERBOSE)
+      auto cstrEnvVar = std::getenv(sName.data());
+      if (!cstrEnvVar)
       {
-         auto str = err.message();
-         PrintError(std::format(L"{} | {}", std::wstring(str.begin(), str.end()), sFilePath));
+         std::print("ERROR | Environment variable '{}' not found\n", sName);
+         return false;
       }
-      return false;
+      std::string sEnvironmentVariableValue(cstrEnvVar);
+      std::filesystem::path pathTmpSysDir(sEnvironmentVariableValue);
+      m_envVars[sName.data()] = pathTmpSysDir;
+      return true;
    }
-   if constexpr (VERBOSE)
+
+   [[nodiscard]] auto GetDirectory(const std::string& sName)
    {
-      Print(std::format(L"Removed: {}", sFilePath));
+      if (m_envVars.contains(sName))
+      {
+         if (std::filesystem::exists(m_envVars[sName]) & std::filesystem::is_directory(m_envVars[sName]))
+         {
+            return m_envVars[sName];
+         }
+         std::print("ERROR | GetDirectory: '{}'");
+      }
+      return std::filesystem::path();
    }
-   return true;
-}
+};
+
+
 
 class JunkEraser
-{   
-   std::vector<std::wstring> m_files;
+{
+public:
+   [[nodiscard]] static bool DeleteFile(std::string_view sFilePath)
+   {
+      static std::error_code err;
+      if (std::filesystem::path pathFile(sFilePath); !std::filesystem::remove(pathFile, err))
+      {
+         if constexpr (VERBOSE) std::print("ERROR | {} | {}\n", err.message(), sFilePath);
+         return false;
+      }
+      if constexpr (VERBOSE) std::print("Removed: {}\n", sFilePath);
+      return true;
+   }
+private:
+   std::vector<std::string> m_files;
 public:
    JunkEraser()
    {
@@ -75,30 +74,35 @@ public:
    {
       if (!FindFiles(directory))
       {
-         Print(std::format(L"No CL files found in {}", directory.wstring()));
+         std::print("No CL files found in {}\n", directory.string());
       }
    }
 
-   bool FindFiles(const std::filesystem::path& directory)
+   size_t FindFiles(const std::filesystem::path& directory)
    {
       uint64_t nTotalJunkSize{};
+      if (directory.empty())
+      {
+         return false;
+      }
       for (const auto& entryTmpFile : std::filesystem::directory_iterator(directory))
       {
          if (entryTmpFile.is_regular_file() && entryTmpFile.path().filename().string().starts_with("_CL_"))
          {
             nTotalJunkSize += entryTmpFile.file_size();
-            m_files.push_back(entryTmpFile.path().wstring());
+            m_files.push_back(entryTmpFile.path().string());
          }
       }
       if constexpr (VERBOSE)
       {
          for (const auto& sTmpFilePath : m_files)
          {
-            Print(sTmpFilePath);
+            std::print("{}\n", sTmpFilePath);
          }
+         std::print("--------------------------------\n");
       }
-      Print(std::format(L"Total size: {} MB | in {} files", nTotalJunkSize/1'048'576, m_files.size()));
-      return static_cast<bool>(m_files.size());
+      std::print("Total size: {} MB | in {} files\n", nTotalJunkSize/1'048'576, m_files.size());
+      return m_files.size();
    }
 
    auto DeleteFoundFiles()
@@ -113,31 +117,31 @@ public:
       }
       return nFilesDeleted;
    }
+
+   [[nodiscard]] auto GetFoundFilesCount() const noexcept
+   {
+      return m_files.size();
+   }
 };
+
+
 
 int main()
 {
-   EnableUnicodeOutput();
-
-   std::filesystem::path pathTmpSysDir;
-   if (!GetPathTmpSysDir(pathTmpSysDir))
-   {
-      return 1;
-   }
-
-   JunkEraser eraser;
-   if (!eraser.FindFiles(pathTmpSysDir))
+   EnvironmentVariableBank bank("TMP");
+   JunkEraser eraser(bank.GetDirectory("TMP"));
+   if (eraser.GetFoundFilesCount() == 0)
    {
       return 0;
    }
-
+   std::print("Delete? (y): ");
    char choice{};
-   std::wcout << L"Delete? (y): ";
    std::cin >> choice;
    if (choice == 'y')
    {
       auto nFilesDeleted = eraser.DeleteFoundFiles();
-      Print(std::format(L"{} files deleted.", nFilesDeleted));
+      std::print("{} files deleted.\n", nFilesDeleted);
+      std::cin >> choice;
    }
    return 0;
 }
