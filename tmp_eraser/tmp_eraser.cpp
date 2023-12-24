77bf079560eaba2b21f5c86365d2b677cf458722
gg-im-done <asketid@gmail.com>
2023-12-24 12:53:55 +0200

    made everything amazing

--- tmp_eraser/tmp_eraser.cpp
+++ tmp_eraser/tmp_eraser.cpp
@@ -1,19 +1,57 @@
-import <iostream>;
-import <string>;
-import <vector>;
-import <filesystem>;
-import <print>;
+#include <conio.h>
+//~
 import <string_view>;
+import <filesystem>;
+import <expected>;
+import <vector>;
+import <string>;
+import <print>;
 import <map>;
 
-static constexpr bool VERBOSE{ true };
+namespace cfg::log
+{
+	static constexpr bool VERBOSE{ false };
+}
 
-
-
+/**
+* @brief A class that stores environment variables
+* 
+* @details
+*    - The environment variable is found by the constructor or by the FindVariable() method
+*    - The environment variable is stored in a map
+*    - The map is indexed by the environment variable name
+*    - The environment variable value is a path (std::filesystem::path) to a directory
+*/
 class EnvironmentVariableBank
 {
    std::map<std::string, std::filesystem::path> m_envVars;
 public:
+	enum class ErrorType : char
+	{
+		NOT_CONTAINED,
+		NOT_EXISTING,
+		NOT_DIRECTORY
+	};
+
+	static constexpr void CheckUnexpected(ErrorType err)
+	{
+		switch (err)
+		{
+		case EnvironmentVariableBank::ErrorType::NOT_CONTAINED:
+			std::println("ERROR | Environment variable does not exist in The Bank");
+			break;
+		case EnvironmentVariableBank::ErrorType::NOT_EXISTING:
+			std::println("ERROR | Environment variable does not exist in the system");
+			break;
+		case EnvironmentVariableBank::ErrorType::NOT_DIRECTORY:
+			std::println("ERROR | Environment variable is not a directory");
+			break;
+		default:
+			std::println("ERROR | UNKNOWN ERROR");
+			break;
+		}
+	}
+
    EnvironmentVariableBank(std::string_view sName)
    {
       FindVariable(sName);
@@ -21,34 +59,42 @@ public:
 
    bool FindVariable(std::string_view sName)
    {
-      auto cstrEnvVar = std::getenv(sName.data());
+      const auto cstrEnvVar = std::getenv(sName.data());
       if (!cstrEnvVar)
       {
-         std::print("ERROR | Environment variable '{}' not found\n", sName);
+         std::println("ERROR | Environment variable '{}' not found", sName);
          return false;
       }
-      std::string sEnvironmentVariableValue(cstrEnvVar);
-      std::filesystem::path pathTmpSysDir(sEnvironmentVariableValue);
+      const std::string sEnvironmentVariableValue(cstrEnvVar);
+      const std::filesystem::path pathTmpSysDir(sEnvironmentVariableValue);
       m_envVars[sName.data()] = pathTmpSysDir;
       return true;
    }
 
-   [[nodiscard]] auto GetDirectory(const std::string& sName)
+   [[nodiscard]] auto GetDirectory(const std::string& sName) -> std::expected<std::filesystem::path, ErrorType>
    {
-      if (m_envVars.contains(sName))
-      {
-         if (std::filesystem::exists(m_envVars[sName]) & std::filesystem::is_directory(m_envVars[sName]))
-         {
-            return m_envVars[sName];
-         }
-         std::print("ERROR | GetDirectory: '{}'", sName);
-      }
-      return std::filesystem::path();
+		if (!m_envVars.contains(sName))
+			return std::unexpected(ErrorType::NOT_CONTAINED);
+		else if (!std::filesystem::exists(m_envVars[sName]))
+			return std::unexpected(ErrorType::NOT_EXISTING);
+		else if (!std::filesystem::is_directory(m_envVars[sName]))
+			return std::unexpected(ErrorType::NOT_DIRECTORY);
+
+      return std::filesystem::path(m_envVars[sName]);
    }
 };
 
-
-
+/**
+* @brief Erases junk files from the temporary directory using the <filesystem> library
+* 
+* @details
+*    - Junk files are files that start with "_CL_" and are located in the temporary directory
+*    - The temporary directory is specified by the environment variable "TMP"
+*    - The environment variable "TMP" is found by the EnvironmentVariableBank
+* 
+* @note
+*   - The temporary directory is usually located at "C:\Users\%USERNAME%\AppData\Local\Temp"
+*/
 class JunkEraser
 {
 public:
@@ -57,10 +103,10 @@ public:
       static std::error_code err;
       if (std::filesystem::path pathFile(sFilePath); !std::filesystem::remove(pathFile, err))
       {
-         if constexpr (VERBOSE) std::print("ERROR | {} | {}\n", err.message(), sFilePath);
+         if constexpr (cfg::log::VERBOSE) std::println("ERROR | {} | {}", err.message(), sFilePath);
          return false;
       }
-      if constexpr (VERBOSE) std::print("Removed: {}\n", sFilePath);
+      if constexpr (cfg::log::VERBOSE) std::println("Removed: {}", sFilePath);
       return true;
    }
 private:
@@ -74,7 +120,7 @@ public:
    {
       if (!FindFiles(directory))
       {
-         std::print("No CL files found in {}\n", directory.string());
+         std::println("No junk files found in [{}]", directory.string());
       }
    }
 
@@ -93,15 +139,16 @@ public:
             m_files.push_back(entryTmpFile.path().string());
          }
       }
-      if constexpr (VERBOSE)
+      if constexpr (cfg::log::VERBOSE)
       {
          for (const auto& sTmpFilePath : m_files)
          {
-            std::print("{}\n", sTmpFilePath);
+            std::println("{}", sTmpFilePath);
          }
          std::print("--------------------------------------------------------------------------------\n");
       }
-      std::print("Total size: {} MB | in {} files\n", nTotalJunkSize/1'048'576, m_files.size());
+		if (m_files.size())
+			std::println("Total size: {} MB | in {} files", nTotalJunkSize/1'048'576, m_files.size());
       return m_files.size();
    }
 
@@ -124,24 +171,47 @@ public:
    }
 };
 
-
+void WeAreDone([[maybe_unused]]const long double o_O = 6e-9l)
+{
+	std::print(R"(
+   _______________                        |*\_/*|________
+  |  ___________  |     .-.     .-.      ||_/-\_|______  |
+  | |           | |    .****. .****.     | |           | |
+  | |   0   0   | |    .*****.*****.     | |   0   0   | |
+  | |     -     | |     .*********.      | |     -     | |
+  | |   \___/   | |      .*******.       | |   \___/   | |
+  | |___     ___| |       .*****.        | |___________| |
+  |_____|\_/|_____|        .***.         |_______________|
+    _|__|/ \|_|_.............*.............._|________|_
+   / ********** \                          / ********** \
+ /  ************  \                      /  ************  \
+--------------------                    --------------------
+)");
+}
 
 int main()
 {
    EnvironmentVariableBank bank("TMP");
-   JunkEraser eraser(bank.GetDirectory("TMP"));
-   if (eraser.GetFoundFilesCount() == 0)
-   {
-      return 0;
-   }
-   std::print("Delete? (y): ");
-   char choice{};
-   std::cin >> choice;
-   if (choice == 'y')
-   {
-      auto nFilesDeleted = eraser.DeleteFoundFiles();
-      std::print("{} files deleted.\n", nFilesDeleted);
-      std::cin >> choice;
-   }
-   return 0;
+   const auto directoryTmp = bank.GetDirectory("TMP");
+	if (directoryTmp.has_value())
+	{
+		JunkEraser eraser(directoryTmp.value());
+		if (eraser.GetFoundFilesCount())
+		{
+			std::println("Delete? (y)");
+			const auto choice = static_cast<char>(_getch());
+			if (choice == 'y')
+			{
+			   const auto nFilesDeleted = eraser.DeleteFoundFiles();
+			   std::println("{} files deleted.", nFilesDeleted);
+			}
+		}
+	}
+	else
+	{
+		EnvironmentVariableBank::CheckUnexpected(directoryTmp.error());
+	}
+
+	WeAreDone();
+	_getch();
 }
